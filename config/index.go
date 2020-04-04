de2b127a6a036b1171c675a28e286d04a7bfbf3a
umuoy1 <burningdian@gmail.com>
2020-04-04 22:32:39 +0800

    删除个人隐私

--- config/index.go
+++ config/index.go
@@ -3,11 +3,11 @@ package config
 
 
 const (
-	CourseId string = "211521843"
-	ClassId string = "24494816"
-	Account string = "18570751245"
-	Pwd string = "li12345"
-	Verify string = "0"
-	Fid string = "1838"
-	Name string = "李一龙"
+	CourseId string = ""
+	ClassId string = ""
+	Account string = ""
+	Pwd string = ""
+	Verify string = ""
+	Fid string = ""
+	Name string = ""
 )
