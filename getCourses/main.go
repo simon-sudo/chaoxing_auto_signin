e6eeda6c7cac40ef7ad23f3af210272c5f5dc1c7
umuoy1 <burningdian@gmail.com>
2020-04-04 23:33:46 +0800

    fix bug

--- getCourses/main.go
+++ getCourses/main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gocolly/colly"
-	"go_dev/chaoxing_auto_signIn/config"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
-
+type Config struct {
+	Account string		`json:"Account"`
+	Pwd string			`json:"Pwd"`
+	Verify string		`json:"Verify"`
+	Fid string			`json:"Fid"`
+	Name string			`json:"Name"`
+}
+type Courses struct {
+	CourseId []string 	`json:"CourseId"`
+	ClassId []string	`json:"ClassId"`
+	CourseName []string	`json:"CourseName"`
+	Config
+}
+var conf Config
+var courses Courses
+func init() {
+	configPath,_ := os.Getwd()
+	configPath = configPath[:strings.LastIndex(configPath,"\\")]
+	configPath += "/config/user.json"
+	data,err := ioutil.ReadFile(configPath)
+	if err!=nil{
+		log.Println(err)
+	}
+	err = json.Unmarshal(data,&conf)
+	courses.Config = conf
+	if err!=nil{
+		log.Println(err)
+	}
+}
 func main() {
 	//登录页
 	const loginUrl = "http://passport2.chaoxing.com/api/login?"
 	loginInfoMap := map[string]string{
-		"name":		config.Name,
-		"pwd": 		config.Pwd,
-		"verify":	config.Verify,
+		"name":		conf.Account,
+		"pwd": 		conf.Pwd,
+		"verify":	conf.Verify,
 	}
 	//登录
 	loginOp := CreateCollector()
@@ -22,7 +52,6 @@ func main() {
 		log.Println("登陆出错：" , err)
 		return
 	}
-	courses := []config.Course{}
 	coursePage := loginOp.Clone()
 	if err!=nil{
 		log.Println(err)
@@ -32,19 +61,24 @@ func main() {
 		s := element.ChildAttrs("input","value")
 		courseName := element.ChildAttr("h3 a","title")
 		if len(s)!=0{
-			courses = append(courses, config.Course{s[0],s[1],courseName})
+			courses.CourseId = append(courses.CourseId, s[0])
+			courses.ClassId = append(courses.ClassId, s[1])
+			courses.CourseName = append(courses.CourseName, courseName)
 		}
 	})
-	file ,err:= os.Create("a.txt")
+	path,_ := os.Getwd()
+	path = path[:strings.LastIndex(path,"\\")]
+	path += "/config/user.json"
+	file ,err:= os.Create(path)
 	if err!=nil{
 		log.Println(err)
 	}
 	err = coursePage.Visit("http://mooc1-1.chaoxing.com/visit/interaction")
-	for _,v := range courses{
-		file.WriteString("courseId="+v.CourseId+"\n")
-		file.WriteString("classId="+v.ClassId+"\n")
-		file.WriteString("courseName="+v.CourseName+"\n\n")
+	coursesStr,err:= json.MarshalIndent(courses,"","   ")
+	if err!=nil{
+		log.Println(err)
 	}
+	file.WriteString(string(coursesStr))
 	file.Close()
 }
 
