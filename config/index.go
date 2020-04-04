0d6b09bcb480e74a661b37ab5f6995e21482620d
umuoy1 <burningdian@gmail.com>
2020-04-04 22:27:25 +0800

    分离课程信息

--- config/index.go
+++ config/index.go
@@ -1,17 +1,13 @@
 package config
 
-type Course struct {
-	CourseId string
-	ClassId string
-	CourseName string
-}
+
 
 const (
-	CourseId string = ""
-	ClassId string = ""
-	Account string = ""
-	Pwd string = ""
+	CourseId string = "211521843"
+	ClassId string = "24494816"
+	Account string = "18570751245"
+	Pwd string = "li12345"
 	Verify string = "0"
-	Fid string = ""
-	Name string = ""
+	Fid string = "1838"
+	Name string = "李一龙"
 )
