f2bf7411dad7d15dfba8b4a35eed0cdf8e6500ae
umuoy1 <burningdian@gmail.com>
2020-04-04 18:21:20 +0800

    删除个人隐私

--- config/index.go
+++ config/index.go
@@ -9,8 +9,9 @@ type Course struct {
 const (
 	CourseId string = "211521843"
 	ClassId string = "24494816"
-	Name string = "18570751245"
+	Account string = "18570751245"
 	Pwd string = "li12345"
 	Verify string = "0"
 	Fid string = "1838"
+	Name string = "李一龙"
 )
